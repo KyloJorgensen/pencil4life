f0cbffd523134cb8e26f2d35c88ac308c73757cf
Kylo Jorgensen <kylo.j.jorgensen@gmail.com>
2018-06-15 10:29:06 -0600

    add babel-polyfill for ie

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.tsx',
+    entry: ['babel-polyfill', './src/index.tsx'],
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'index_bundle.js'
