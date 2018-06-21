e3522c95f8a4ecee13c74b198808053c55073a05
Kylo Jorgensen <kylo.j.jorgensen@gmail.com>
2018-06-21 13:28:04 -0600

    copied app over and removed old

--- www/server/routes/report-violation/report-violation.router.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var express = require('express'),
-	router = express.Router(),
-	controller = require('./report-violation.controller.js');
-
-router.post('/', controller.reportViolation)
-
-module.exports = router;
--- www/test/test-endpoints.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-var testMainEndpoint = require('./endpoints/test-main-endpoint')();
