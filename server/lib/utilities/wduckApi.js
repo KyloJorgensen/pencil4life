0385cee61db860b5556e430776f2daf1d90398c7
Kylo Jorgensen <kylo.j.jorgensen@gmail.com>
2018-07-23 12:52:42 -0600

    made newUser and getUserId

--- server/lib/utilities/wduckApi.js
+++ server/lib/utilities/wduckApi.js
@@ -20,10 +20,52 @@ exports.fetchFromWDuckApi = function (endpoint, requestInit) {
         return info;
     });
 };
-exports.submitMessage = function (submitMessageOptions, callback) {
+exports.getUserId = function (username) {
     return exports.fetchFromWDuckApi('/users')
         .then(function (users) {
-        console.log(users);
+        if (users.success) {
+            var userindex = users.results.findIndex(function (user) {
+                return user.username == username;
+            });
+            if (userindex != -1) {
+                return users.results[userindex].id;
+            }
+            return null;
+        }
+        else {
+            throw new Error('Failed to get users');
+        }
+    });
+};
+exports.newUser = function (username, newUserOptions) {
+    return exports.fetchFromWDuckApi('/users', {
+        method: 'POST',
+        body: JSON.stringify(newUserOptions),
+    })
+        .then(function (user) {
+        console.log(user);
+        if (user.success == true) {
+            return user.id;
+        }
+        else {
+            throw new Error(user.error);
+        }
+    });
+};
+exports.submitMessage = function (submitMessageOptions, callback) {
+    var username = 'noreply';
+    return exports.getUserId(username)
+        .then(function (userId) {
+        if (userId) {
+            return userId;
+        }
+        else {
+            return exports.newUser(username, {
+                name: username,
+                password: false,
+                receivedMax: 0,
+            });
+        }
     });
     // return fetchFromWDuckApi(`/users/${userId}/submit`, {
     //   method: 'POST',
